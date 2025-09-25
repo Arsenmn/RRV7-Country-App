f126601214cfe3b1839dbbe6eb9bc5b5046be98f
Arsen Yergali <ergali.arsenn@gmail.com>
2025-09-25 16:47:53 +0500

    FV

--- app/routes/countries.tsx
+++ app/routes/countries.tsx
@@ -30,12 +30,12 @@ export default function Countries({ loaderData }: Route.ComponentProps) {
           placeholder="Search by name..."
           value={search}
           onChange={(e) => setSearch(e.target.value)}
-          className="border border-gray-300 rounded px-3 py-2 w-full sm:w-1/2 focus:outline-none focus:border-indigo-500"
+          className="border border-gray-300 rounded px-3 py-2 w-full sm:w-1/2 focus:outline-none focus:border-green-600"
         />
         <select
           value={region}
           onChange={(e) => setRegion(e.target.value)}
-          className="border border-gray-300 rounded px-3 py-2 w-full sm:w-1/2 focus:outline-none focus:border-indigo-500"
+          className="border border-gray-300 rounded px-3 py-2 w-full sm:w-1/2 focus:outline-none focus:border-green-600"
         >
           <option value="">All Regions</option>
           <option value="africa">Africa</option>
